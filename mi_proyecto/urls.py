23b4eb25f7153601cab29bc4ba30e14c13c0717f
Carlos Yañez <xdcarlostobenyixd@gmail.com>
2026-08-18 13:19:31 -0400

    feat: hola mundo en django

--- mi_proyecto/urls.py
+++ mi_proyecto/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from bienvenida.views import inicio
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('inicio/', view=inicio, name='inicio'),
 ]
